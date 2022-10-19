869013d081555e47b645a1f56b86be8918b93c35
Carlos Costa <up202004151@up.pt>
2022-10-19 12:44:02 +0100

    Trying Step 8

--- src/main/java/com/aor/numbers/ListFilterer.java
+++ src/main/java/com/aor/numbers/ListFilterer.java
@@ -1,4 +1,18 @@
 package com.aor.numbers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListFilterer {
+    private final GenericListFilter filter;
+
+    public ListFilterer(GenericListFilter filter){
+        this.filter = filter;
+    }
+    public List<Integer> filter(List<Integer> list){
+        List<Integer> filtrado = new ArrayList<>();
+        for (int i : list)
+            if (filter.accept(i)) filtrado.add(i);
+        return filtrado;
+    }
 }
